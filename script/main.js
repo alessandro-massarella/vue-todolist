6aa4bfc41b807e5951e3be7c5a12105c25ac22dd
alessandro <alessandro.massarella@gmail.com>
2020-11-19 16:56:17 +0100

    1.0 (aggiunta bonus)

--- script/main.js
+++ script/main.js
@@ -3,17 +3,29 @@ const myToDo = new Vue ({
   data: {
     daFareArray: [],
     message: '',
+    // array per lo stato del li:
+    areActive: []
   },
   methods: {
     addToArray: function(){
-      this.daFareArray.push(this.message)
-    },
-    removeMessage: function(delIndex){
-      this.daFareArray.splice(delIndex, 1);
-    },
-    resetWrite: function(){
+      this.daFareArray.push(this.message);
+      this.areActive.push(false);
       this.message = "";
     },
+    removeMessage: function(index){
+      // SPLICE: cosa cancellare e a partire da lui quanti?:
+      this.daFareArray.splice(index, 1);
+      this.areActive.splice(index, 1);
+    },
+    // resetWrite: function(){
+    //   this.message = "";
+    // },
+    toggleClass(index){
+      // caveats:
+      Vue.set(this.areActive, index,
+        // per invertire il valore:
+        !this.areActive[index]);
+    }
 
   }
 })
